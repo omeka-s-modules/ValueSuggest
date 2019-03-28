04c31996aed1e11a7e1505241e3b7da01d5e9b5e
Jim Safley <jimsafley@gmail.com>
2019-03-28 15:33:40 -0400

    Add docs

--- src/Suggester/SuggesterInterface.php
+++ src/Suggester/SuggesterInterface.php
@@ -6,8 +6,25 @@ interface SuggesterInterface
     /**
      * Get an array of suggestions given a query.
      *
-     * @param string $query
-     * @param string $lang
+     * Implementations should return an array in the following format:
+     *
+     * [
+     *   [
+     *     'value' => <value>,
+     *     'data' => [
+     *       'uri' => <uri>,
+     *       'info' => <info>,
+     *     ],
+     *   ],
+     *   <...>
+     * ]
+     *
+     * <value>: (required) the suggestion text, preferably in the passed language
+     * <uri>: (optional) the suggestion's canonical URI
+     * <info>: (optional) any disambiguating text, preferably in the passed language
+     *
+     * @param string $query The query
+     * @param string $lang The language code
      * @return array
      */
     public function getSuggestions($query, $lang = null);
