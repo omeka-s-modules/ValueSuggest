896b1fcf1a4c2e529d964df24a5ca2cc704077cc
Jim Safley <jimsafley@gmail.com>
2019-04-12 16:31:23 -0400

    Ad UNISCO info

--- src/Suggester/Unesco/Sparql.php
+++ src/Suggester/Unesco/Sparql.php
@@ -35,19 +35,26 @@ class Sparql implements SuggesterInterface
     public function getSuggestions($query, $lang = null)
     {
         $sparqlQuery = sprintf('
-SELECT ?Subject ?Label
+SELECT ?Subject ?Label ?AltLabel ?Broader
 WHERE {
   GRAPH <%s> {
     ?Subject skos:prefLabel ?Label ;
     rdf:type skos:Concept .
+    OPTIONAL {?Subject skos:altLabel ?AltLabel}
+    OPTIONAL {?Subject skos:broader [skos:prefLabel ?Broader]}
     FILTER regex(?Label, "%s", "i")
     FILTER langMatches(lang(?Label), "%s")
+    FILTER langMatches(lang(?AltLabel), "%s")
+    FILTER langMatches(lang(?Broader), "%s")
   }
 }
 LIMIT 500',
             addslashes($this->graph),
             addslashes($query),
-            addslashes($lang) ?: 'es' // The defualt lang is spanish
+            // The defualt lang is spanish.
+            addslashes($lang) ?: 'es',
+            addslashes($lang) ?: 'es',
+            addslashes($lang) ?: 'es'
         );
 
         $client = $this->client->setUri(self::ENDPOINT)->setParameterGet([
@@ -62,11 +69,18 @@ LIMIT 500',
         $suggestions = [];
         $results = json_decode($response->getBody(), true);
         foreach ($results['results']['bindings'] as $result) {
+            $info = [];
+            if (isset($result['Broader']['value'])) {
+                $info[] = $result['Broader']['value'];
+            }
+            if (isset($result['AltLabel']['value'])) {
+                $info[] = $result['AltLabel']['value'];
+            }
             $suggestions[] = [
                 'value' => $result['Label']['value'],
                 'data' => [
                     'uri' => $result['Subject']['value'],
-                    'info' => null,
+                    'info' => implode(' > ', $info),
                 ],
             ];
         }
