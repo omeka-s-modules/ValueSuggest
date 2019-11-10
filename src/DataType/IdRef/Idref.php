af1796766f2c5fae30e25e020e4c0986d4690620
Daniel Berthereau <Daniel.github@Berthereau.net>
2019-11-11 00:00:00 +0100

    Included IdRef.

---
+++ src/DataType/IdRef/Idref.php
@@ -0,0 +1,23 @@
+<?php
+namespace ValueSuggest\DataType\IdRef;
+
+use ValueSuggest\DataType\AbstractDataType;
+use ValueSuggest\Suggester\IdRef\IdRefSuggest;
+
+class IdRef extends AbstractDataType
+{
+    public function getSuggester()
+    {
+        return new IdRefSuggest($this->services->get('Omeka\HttpClient'));
+    }
+
+    public function getName()
+    {
+        return 'valuesuggest:idref:all';
+    }
+
+    public function getLabel()
+    {
+        return 'IdRef: The French national database for research'; // @translate
+    }
+}
