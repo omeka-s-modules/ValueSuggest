fd110c5d6181c1b73e33c158dcbbd2e1a1192d36
Jim Safley <jimsafley@gmail.com>
2021-09-10 06:38:07 -0400

    Finish adding vocabularies

--- src/Service/McDataTypeFactory.php
+++ src/Service/McDataTypeFactory.php
@@ -96,10 +96,74 @@ class McDataTypeFactory implements FactoryInterface
             'label' => "Ministère de la Culture: Liste d'autorités Époques - Joconde", // @translate
             'scheme' => 'http://data.culture.fr/thesaurus/resource/ark:/67717/T93',
         ],
-        // 'valuesuggestall:mc:' => [
-        //     'label' => "Ministère de la Culture: ", // @translate
-        //     'scheme' => '',
-        // ],
+        'valuesuggestall:mc:T20' => [
+            'label' => "Ministère de la Culture: Matériau de la couverture - Inventaire/MH", // @translate
+            'scheme' => 'http://data.culture.fr/thesaurus/resource/ark:/67717/T20',
+        ],
+        'valuesuggestall:mc:T57' => [
+            'label' => "Ministère de la Culture: Matériau du gros-oeuvre et mise en oeuvre - Inventaire/MH", // @translate
+            'scheme' => 'http://data.culture.fr/thesaurus/resource/ark:/67717/T57',
+        ],
+        'valuesuggestall:mc:T94' => [
+            'label' => "Ministère de la Culture: Matériaux et techniques du patrimoine mobilier - Inventaire/MH", // @translate
+            'scheme' => 'http://data.culture.fr/thesaurus/resource/ark:/67717/T94',
+        ],
+        'valuesuggestall:mc:0404efce-2024-4694-bf80-eba4fc2b336c' => [
+            'label' => "Ministère de la Culture: Nomenclatures HADOC", // @translate
+            'scheme' => 'http://data.culture.fr/thesaurus/resource/ark:/67717/0404efce-2024-4694-bf80-eba4fc2b336c',
+        ],
+        'valuesuggestall:mc:98b3feb3-744b-4ba3-8381-18ca4a555a4c' => [
+            'label' => "Ministère de la Culture: Référentiel de communicabilité des archives publiques", // @translate
+            'scheme' => 'http://data.culture.fr/thesaurus/resource/ark:/67717/98b3feb3-744b-4ba3-8381-18ca4a555a4c',
+        ],
+        'valuesuggestall:mc:T532' => [
+            'label' => "Ministère de la Culture: Stade de la création des objets mobiliers - Inventaire/MH", // @translate
+            'scheme' => 'http://data.culture.fr/thesaurus/resource/ark:/67717/T532',
+        ],
+        'valuesuggestall:mc:T59' => [
+            'label' => "Ministère de la Culture: Statut de la propriété des Biens culturels - Inventaire/MH", // @translate
+            'scheme' => 'http://data.culture.fr/thesaurus/resource/ark:/67717/T59',
+        ],
+        'valuesuggestall:mc:T61' => [
+            'label' => "Ministère de la Culture: Techniques photographiques", // @translate
+            'scheme' => 'http://data.culture.fr/thesaurus/resource/ark:/67717/T61',
+        ],
+        'valuesuggestall:mc:T69' => [
+            'label' => "Ministère de la Culture: Thésaurus de la désignation des objets mobiliers", // @translate
+            'scheme' => 'http://data.culture.fr/thesaurus/resource/ark:/67717/T69',
+        ],
+        'valuesuggestall:mc:T96' => [
+            'label' => "Ministère de la Culture: Thésaurus de la désignation des œuvres architecturales et des espaces aménagés", // @translate
+            'scheme' => 'http://data.culture.fr/thesaurus/resource/ark:/67717/T96',
+        ],
+        'valuesuggestall:mc:Matiere' => [
+            'label' => "Ministère de la Culture: Thésaurus-matières pour l'indexation des archives locales", // @translate
+            'scheme' => 'http://data.culture.fr/thesaurus/resource/ark:/67717/Matiere',
+        ],
+        'valuesuggestall:mc:T26' => [
+            'label' => "Ministère de la Culture: Type de la couverture - Inventaire/MH", // @translate
+            'scheme' => 'http://data.culture.fr/thesaurus/resource/ark:/67717/T26',
+        ],
+        'valuesuggestall:mc:T10' => [
+            'label' => "Ministère de la Culture: Type de protection MH - Inventaire/MH", // @translate
+            'scheme' => 'http://data.culture.fr/thesaurus/resource/ark:/67717/T10',
+        ],
+        'valuesuggestall:mc:51232822-adac-4a33-aa14-29e2c701a5ee' => [
+            'label' => "Ministère de la Culture: Vocabulaire des activités des entités productrices d'archives", // @translate
+            'scheme' => 'http://data.culture.fr/thesaurus/resource/ark:/67717/51232822-adac-4a33-aa14-29e2c701a5ee',
+        ],
+        'valuesuggestall:mc:37684874-0820-402d-b53e-f8ab6b9ed8ba' => [
+            'label' => "Ministère de la Culture: Vocabulaire des altérations", // @translate
+            'scheme' => 'http://data.culture.fr/thesaurus/resource/ark:/67717/37684874-0820-402d-b53e-f8ab6b9ed8ba',
+        ],
+        'valuesuggestall:mc:f14e8183-5885-46d6-8fc9-17ebd8f3c27e' => [
+            'label' => "Ministère de la Culture: Vocabulaire des domaines d'action ou objets des entités productrices d'archives", // @translate
+            'scheme' => 'http://data.culture.fr/thesaurus/resource/ark:/67717/f14e8183-5885-46d6-8fc9-17ebd8f3c27e',
+        ],
+        'valuesuggestall:mc:2012b973-ddb2-4540-a775-9157c3c1d7fd' => [
+            'label' => "Ministère de la Culture: Vocabulaire pour les techniques photographiques", // @translate
+            'scheme' => 'http://data.culture.fr/thesaurus/resource/ark:/67717/2012b973-ddb2-4540-a775-9157c3c1d7fd',
+        ],
     ];
 
     public function __invoke(ContainerInterface $services, $requestedName, array $options = null)
