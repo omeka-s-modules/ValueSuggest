3add396680a5070298fd9f9523eb53c65e426db3
Jim Safley <jimsafley@gmail.com>
2021-09-10 05:57:12 -0400

    Add more vocabularies

--- src/Service/McDataTypeFactory.php
+++ src/Service/McDataTypeFactory.php
@@ -9,7 +9,7 @@ class McDataTypeFactory implements FactoryInterface
 {
     protected $types = [
         'valuesuggestall:mc:T262' => [
-            'label' => "Ministère de la Culture: Catégories techniques et domaines", // @translate
+            'label' => "Ministère de la Culture: Catégories techniques et domaines - Inventaire/MH", // @translate
             'scheme' => 'http://data.culture.fr/thesaurus/resource/ark:/67717/T262',
         ],
         'valuesuggestall:mc:01952848-4419-431e-8e81-419c1d46b30d' => [
@@ -17,11 +17,11 @@ class McDataTypeFactory implements FactoryInterface
             'scheme' => 'http://data.culture.fr/thesaurus/resource/ark:/67717/01952848-4419-431e-8e81-419c1d46b30d',
         ],
         'valuesuggestall:mc:T124' => [
-            'label' => "Ministère de la Culture: Etat de conservation du patrimoine mobilier", // @translate
+            'label' => "Ministère de la Culture: Etat de conservation du patrimoine mobilier - Inventaire/MH", // @translate
             'scheme' => 'http://data.culture.fr/thesaurus/resource/ark:/67717/T124',
         ],
         'valuesuggestall:mc:T529' => [
-            'label' => "Ministère de la Culture: Inscriptions, marques, emblématique et poinçons", // @translate
+            'label' => "Ministère de la Culture: Inscriptions, marques, emblématique et poinçons - Inventaire/MH", // @translate
             'scheme' => 'http://data.culture.fr/thesaurus/resource/ark:/67717/T529',
         ],
         'valuesuggestall:mc:T2' => [
@@ -35,6 +35,66 @@ class McDataTypeFactory implements FactoryInterface
         'valuesuggestall:mc:T3' => [
             'label' => "Ministère de la Culture: Liste d'autorité Typologie documentaire pour l'indexation des archives locales", // @translate
             'scheme' => 'http://data.culture.fr/thesaurus/resource/ark:/67717/T3',
+        ],
+        'valuesuggestall:mc:T513' => [
+            'label' => "Ministère de la Culture: Liste d'autorités Auteurs - Joconde", // @translate
+            'scheme' => 'http://data.culture.fr/thesaurus/resource/ark:/67717/T513',
+        ],
+        'valuesuggestall:mc:T51' => [
+            'label' => "Ministère de la Culture: Liste d'autorités Domaines - Joconde", // @translate
+            'scheme' => 'http://data.culture.fr/thesaurus/resource/ark:/67717/T51',
+        ],
+        'valuesuggestall:mc:T115' => [
+            'label' => "Ministère de la Culture: Liste d'autorités Découverte - Joconde", // @translate
+            'scheme' => 'http://data.culture.fr/thesaurus/resource/ark:/67717/T115',
+        ],
+        'valuesuggestall:mc:T505' => [
+            'label' => "Ministère de la Culture: Liste d'autorités Dénomination - Joconde", // @translate
+            'scheme' => 'http://data.culture.fr/thesaurus/resource/ark:/67717/T505',
+        ],
+        'valuesuggestall:mc:T506' => [
+            'label' => "Ministère de la Culture: Liste d'autorités Genèse - Joconde", // @translate
+            'scheme' => 'http://data.culture.fr/thesaurus/resource/ark:/67717/T506',
+        ],
+        'valuesuggestall:mc:T520' => [
+            'label' => "Ministère de la Culture: Liste d'autorités Inscriptions - Joconde", // @translate
+            'scheme' => 'http://data.culture.fr/thesaurus/resource/ark:/67717/T520',
+        ],
+        'valuesuggestall:mc:T84' => [
+            'label' => "Ministère de la Culture: Liste d'autorités Lieux - Joconde", // @translate
+            'scheme' => 'http://data.culture.fr/thesaurus/resource/ark:/67717/T84',
+        ],
+        'valuesuggestall:mc:T515' => [
+            'label' => "Ministère de la Culture: Liste d'autorités Localisation - Joconde", // @translate
+            'scheme' => 'http://data.culture.fr/thesaurus/resource/ark:/67717/T515',
+        ],
+        'valuesuggestall:mc:T521' => [
+            'label' => "Ministère de la Culture: Liste d'autorités Périodes - Joconde", // @translate
+            'scheme' => 'http://data.culture.fr/thesaurus/resource/ark:/67717/T521',
+        ],
+        'valuesuggestall:mc:T523' => [
+            'label' => "Ministère de la Culture: Liste d'autorités Représentation - Joconde", // @translate
+            'scheme' => 'http://data.culture.fr/thesaurus/resource/ark:/67717/T523',
+        ],
+        'valuesuggestall:mc:T507' => [
+            'label' => "Ministère de la Culture: Liste d'autorités Sources de la représentation - Joconde", // @translate
+            'scheme' => 'http://data.culture.fr/thesaurus/resource/ark:/67717/T507',
+        ],
+        'valuesuggestall:mc:T516' => [
+            'label' => "Ministère de la Culture: Liste d'autorités Techniques – Joconde", // @translate
+            'scheme' => 'http://data.culture.fr/thesaurus/resource/ark:/67717/T516',
+        ],
+        'valuesuggestall:mc:T86' => [
+            'label' => "Ministère de la Culture: Liste d'autorités Utilisation - Joconde", // @translate
+            'scheme' => 'http://data.culture.fr/thesaurus/resource/ark:/67717/T86',
+        ],
+        'valuesuggestall:mc:T517' => [
+            'label' => "Ministère de la Culture: Liste d'autorités Écoles - Joconde", // @translate
+            'scheme' => 'http://data.culture.fr/thesaurus/resource/ark:/67717/T517',
+        ],
+        'valuesuggestall:mc:T93' => [
+            'label' => "Ministère de la Culture: Liste d'autorités Époques - Joconde", // @translate
+            'scheme' => 'http://data.culture.fr/thesaurus/resource/ark:/67717/T93',
         ],
         // 'valuesuggestall:mc:' => [
         //     'label' => "Ministère de la Culture: ", // @translate
