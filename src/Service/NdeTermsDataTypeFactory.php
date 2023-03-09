202bbe56bccd5c8b7b90de85cf752bd7f5b0f19f
Bob Coret <bob.coret+github@gmail.com>
2023-03-09 16:24:01 +0100

    Added NDE sources
    Added sources from the Network of Terms Catalog (https://github.com/netwerk-digitaal-erfgoed/network-of-terms/tree/master/packages/network-of-terms-catalog/catalog/datasets)

--- src/Service/NdeTermsDataTypeFactory.php
+++ src/Service/NdeTermsDataTypeFactory.php
@@ -12,9 +12,25 @@ use ValueSuggest\DataType\NdeTerms\NdeTerms;
 class NdeTermsDataTypeFactory implements FactoryInterface
 {
     protected $types = [
+        'valuesuggest:ndeterms:aatm' => [
+            'label' => 'NDE: Art & Architecture Thesaurus - materialen', // @translate
+            'source' => 'http://vocab.getty.edu/aat/sparql/materials',
+        ],
+        'valuesuggest:ndeterms:aatpt' => [
+            'label' => 'NDE: Art & Architecture Thesaurus - processen en technieken', // @translate
+            'source' => 'http://vocab.getty.edu/aat/sparql/processes-and-techniques',
+        ],
+        'valuesuggest:ndeterms:aatsp' => [
+            'label' => 'NDE: Art & Architecture Thesaurus - stijlen en periodes', // @translate
+            'source' => 'http://vocab.getty.edu/aat/sparql/styles-and-periods',
+        ],
         'valuesuggest:ndeterms:abr' => [
             'label' => 'NDE: Archeologisch Basisregister', // @translate
             'source' => 'https://data.cultureelerfgoed.nl/PoolParty/sparql/term/id/abr',
+        ],
+        'valuesuggest:ndeterms:adamlink' => [
+            'label' => 'NDE: Adamlink: straten in Amsterdam', // @translate
+            'source' => 'https://druid.datalegend.net/AdamNet/Geography/sparql#streets',
         ],
         'valuesuggest:ndeterms:btt' => [
             'label' => 'NDE: Brinkman trefwoordenthesaurus', // @translate
@@ -24,6 +40,26 @@ class NdeTermsDataTypeFactory implements FactoryInterface
             'label' => 'NDE: Cultuurhistorische Thesaurus', // @translate
             'source' => 'https://data.cultureelerfgoed.nl/PoolParty/sparql/term/id/cht',
         ],
+        'valuesuggest:ndeterms:chtm' => [
+            'label' => 'NDE: Cultuurhistorische Thesaurus - Materialen', // @translate
+            'source' => 'https://data.cultureelerfgoed.nl/PoolParty/sparql/term/id/cht/materials',
+        ],
+        'valuesuggest:ndeterms:chtsp' => [
+            'label' => 'NDE: Cultuurhistorische Thesaurus - Stijlen en periodes', // @translate
+            'source' => 'https://data.cultureelerfgoed.nl/PoolParty/sparql/term/id/cht/styles-and-periods',
+        ],
+        'valuesuggest:ndeterms:eurovoc' => [
+            'label' => 'NDE: EuroVoc - thesaurus van de Europese Unie', // @translate
+            'source' => 'http://publications.europa.eu/webapi/rdf/sparql#eurovoc',
+        ],
+        'valuesuggest:ndeterms:geonames' => [
+            'label' => 'NDE: GeoNames: geografische namen in Nederland, België en Duitsland', // @translate
+            'source' => 'https://demo.netwerkdigitaalerfgoed.nl/geonames',
+        ],
+        'valuesuggest:ndeterms:gtm' => [
+            'label' => 'NDE: Goudse straten', // @translate
+            'source' => 'https://www.goudatijdmachine.nl/id/straten#streets',
+        ],		
         'valuesuggest:ndeterms:gtaagen' => [
             'label' => 'NDE: GTAA: genres', // @translate
             'source' => 'https://data.beeldengeluid.nl/id/datadownload/0028',
@@ -31,6 +67,18 @@ class NdeTermsDataTypeFactory implements FactoryInterface
         'valuesuggest:ndeterms:gtaaond' => [
             'label' => 'NDE: GTAA: onderwerpen', // @translate
             'source' => 'https://data.beeldengeluid.nl/id/datadownload/0031',
+        ],
+        'valuesuggest:ndeterms:homosaurus' => [
+            'label' => 'NDE: Homosaurus', // @translate
+            'source' => 'https://data.ihlia.nl/PoolParty/sparql/homosaurus',
+        ],
+        'valuesuggest:ndeterms:iconclass' => [
+            'label' => 'NDE: Iconclass', // @translate
+            'source' => 'https://iconclass.org/sparql',
+        ],
+        'valuesuggest:ndeterms:ied' => [
+            'label' => 'NDE: Indisch Erfgoed Thesaurus', // @translate
+            'source' => 'https://digitaalerfgoed.poolparty.biz/PoolParty/sparql/ied',
         ],
         'valuesuggest:ndeterms:muzgs' => [
             'label' => 'NDE: Muziek: genres en stijlen', // @translate
@@ -51,6 +99,10 @@ class NdeTermsDataTypeFactory implements FactoryInterface
         'valuesuggest:ndeterms:rkdartists' => [
             'label' => 'NDE: RKDartists', // @translate
             'source' => 'https://data.netwerkdigitaalerfgoed.nl/rkd/rkdartists/sparql',
+        ],
+        'valuesuggest:ndeterms:stcn' => [
+            'label' => 'NDE: STCN: drukkers', // @translate
+            'source' => 'http://data.bibliotheken.nl/thes/drukkers/sparql',
         ],
         'valuesuggest:ndeterms:tnmw' => [
             'label' => 'NDE: Thesaurus Nationaal Museum van Wereldculturen', // @translate
