7a88a91c7a0a66421f1bff86e2a23a8e1a0ad91c
Maarten Coonen <m.coonen@maastrichtuniversity.nl>
2025-04-02 15:24:12 +0200

    Fix ViafSuggest by 1.) switching to HTTPS, 2.) using cURL adapter and 3.) enter valid Accept-Encoding values

--- src/Suggester/Oclc/ViafSuggest.php
+++ src/Suggester/Oclc/ViafSuggest.php
@@ -19,17 +19,31 @@ class ViafSuggest implements SuggesterInterface
     /**
      * Retrieve suggestions from the VIAF auto suggest service.
      *
-     * @see https://platform.worldcat.org/api-explorer/apis/VIAF
+     * @see https://developer.api.oclc.org/viaf-api
      * @param string $query
      * @param string $lang
      * @return array
      */
     public function getSuggestions($query, $lang = null)
     {
+        $params = [
+            'query' => $query,
+        ];
+
+        // The VIAF API is hosted on Cloudflare. The default Laminas\Http\Client\Adapter\Socket triggers a
+        // Cloudflare "security service that protect itself from online attacks", i.e. automated traffic.
+        // Let's use a fallback to the cURL adapter, as that is compatible with the Cloudflare hosted VIAF API.
         $response = $this->client
-            ->setUri('http://www.viaf.org/viaf/AutoSuggest')
-            ->setParameterGet(['query' => $query])
+            ->setAdapter('Laminas\Http\Client\Adapter\Curl')    // Fallback to cURL
+            ->setUri('https://www.viaf.org/viaf/AutoSuggest')
+            ->setHeaders([
+                // Override some default headers set by Laminas\Http\Client
+                'Accept-Encoding' => 'deflate, br, identity',   // 'gzip' is not supported
+                'Accept' => 'application/json',
+            ])
+            ->setParameterGet($params)
             ->send();
+
         if (!$response->isSuccess()) {
             return [];
         }
